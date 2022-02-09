269799513a0c6a3c9bd62ab229a50cb798cb606a
IT21341540 <99330301+IT21341540@users.noreply.github.com>
2022-02-09 17:51:03 +0530

    Update Tute01.c

--- Tute01.c
+++ Tute01.c
@@ -5,7 +5,18 @@
 #include <stdio.h>
 
 int main() {
-  
+float no1, no2;
+float avg, mark1, mark2;
+sum = 0;
+printf("Enter mark 1");
+scanf("%f", &mark);
+printf("Enter mark 2");
+scanf("%f", &mark2):
+sum = mark 1 + mark 2;
+avg = sum/2;
+printf("This is sum");
+printf("This is average");
+   
   return 0;
 }
 
